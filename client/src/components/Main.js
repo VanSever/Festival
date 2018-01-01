cea4fad408ca5d4305d3b0c7df2b4e0931d2ce98
Lennert Van sever <lennert_vansever@hotmail.be>
2018-01-01 23:39:23 +0100

    initial setup

---
+++ client/src/components/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import '../style/css/main.css';
+
+
+class Main extends Component {
+  render() {
+  	return (
+  		<div className="content">
+  			{/*<Route path="/forms/login/:facebook_id" component={Login} />*/}
+  		</div>
+	  );
+  }
+}
+
+export default Main;
