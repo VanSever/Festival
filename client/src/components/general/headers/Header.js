1c065fe30d5449d6535456c600af07ed8986863a
Lennert Van sever <lennert_vansever@hotmail.be>
2018-03-17 01:03:51 +0100

    dynamic header

---
+++ client/src/components/general/headers/Header.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	returnBack(event){
+		event.preventDefault();
+		this.props.history.goBack();
+	}
+	generateBackButton(){
+		if(this.props.backButton){
+			return (
+				<form onSubmit={(event) => this.returnBack(event)} className="back_button_form">
+					<input
+						type="image"
+						src="/icons/back_button.svg"
+					/>
+				</form>
+			)
+		}
+	}
+	generateTitle(){
+		if(this.props.title){
+			return <h1>{this.props.title}</h1>;
+		}
+	}
+	generateRightButton(){
+		if(this.props.like){
+			return(
+				<form onSubmit={(event) => this.returnBack(event)} className="right_button_form">
+					<input
+						type="image"
+						src="/icons/like_button.svg"
+					/>
+				</form>
+			)
+		}
+	}
+	render() {
+		return (
+      <div className="header">
+				{this.generateBackButton()}
+				{this.generateTitle()}
+				{this.generateRightButton()}
+      </div>
+    );
+	}
+}
+
+export default Header;
